7679fbb95f6983d3d787ca8d5eecae2a7b648343
JS <kw0095@naver.com>
2021-02-03 17:12:21 +0900

    final test

--- pod2/container.js
+++ pod2/container.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 var app = express();
 app.get('/',function(req,res){
-	res.send("hello i'm container!\n");
+	res.send("hello i'm in pod2!\n");
 });
-app.get('/pod1',function(req,res){
-	res.send("hello ingress I'm in pod1!\n");
+app.get('/pod2',function(req,res){
+	res.send("hello ingress I'm in pod2!\n");
 });
 app.listen(3000,function(){
 	console.log("successfully listen at 3000 port!\n");
